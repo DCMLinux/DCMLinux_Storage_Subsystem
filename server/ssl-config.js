fdfba93e013bd49d37fbf31116d454027830edf3
Jorge Fernandez <jorge.fernandez@kapa7.com>
2021-04-13 21:28:35 -0600

    Fix to use docker self signed certs

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -1,4 +1,4 @@
 var path = require('path'),
 fs = require("fs");
-exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privatekey.pem')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, './private/certificate.pem')).toString();
+exports.privateKey = fs.readFileSync('/dcmlinux/certs/selfsigned/selfsigned.key').toString();
+exports.certificate = fs.readFileSync('/dcmlinux/certs/selfsigned/selfsigned.cert').toString();
